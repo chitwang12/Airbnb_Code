2e0bfb02b4afedc3a723de26b3d210f1c4262116
Chitwan Gandhi <chitwangandhi@Chitwans-MacBook-Air.local>
2025-05-03 02:50:45 +0530

    Sequelize : Added hotel models

---
+++ src/db/models/hotel.ts
@@ -0,0 +1,61 @@
+import { CreationOptional, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import  sequelize  from "./sequelize"
+
+
+class Hotel extends Model<InferAttributes<Hotel>, InferCreationAttributes<Hotel>> {
+
+    declare id: CreationOptional<number>;
+    declare name: string;
+    declare address: string;
+    declare location: string;
+    declare created_at: CreationOptional<Date>;
+    declare updated_at: CreationOptional<Date>;
+    declare rating: number;
+    declare rating_count: number;
+}
+
+
+Hotel.init({
+    id: {
+        type: 'int',
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: 'varchar',
+        allowNull: false,
+    },
+    address: {
+        type: 'varchar',
+        allowNull: false,
+    },
+    location: {
+        type: 'varchar',
+        allowNull: false,
+    },
+    created_at: {
+        type: 'timestamp',
+        defaultValue: new Date(),
+    },
+    updated_at: {
+        type: 'timestamp',
+        defaultValue: new Date(),
+    },
+    rating: {
+        type: 'decimal',
+        defaultValue: null,
+    },
+    rating_count: {
+        type: 'int',
+        defaultValue: null,
+    }
+}, {
+    tableName: 'hotels',
+    sequelize: sequelize,
+    underscored: true,
+    timestamps: false,
+});
+
+
+export default Hotel;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
-import express from 'express';
-import { serverConfig }from './config';
-import v1Router from  './routers/v1/index.router';
-import v2Router from  './routers/v2/index.router';
-import { genericErrorHandler } from './middlewares/error.middleware';
-import logger from './config/logger.config';
-import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import express from "express";
+import { serverConfig } from "./config";
+import v1Router from "./routers/v1/index.router";
+import v2Router from "./routers/v2/index.router";
+import { genericErrorHandler } from "./middlewares/error.middleware";
+import logger from "./config/logger.config";
+import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
+import sequelize from "./db/models/sequelize";
 const app = express();
-
 
 app.use(express.json());
 
 /**
  * Registering all the router and their corresponding routes without app server object.
  */
-app.use(attachCorrelationIdMiddleware)
-app.use('/api/v1', v1Router);
-app.use('/api/v2', v2Router);
-
+app.use(attachCorrelationIdMiddleware);
+app.use("/api/v1", v1Router);
+app.use("/api/v2", v2Router);
 
 // After all the routing Middlewares are handled, we can add the error handler middleware
 /**
@@ -24,7 +23,25 @@ app.use('/api/v2', v2Router);
  */
 app.use(genericErrorHandler);
 
-app.listen(serverConfig.PORT, () => {
-    logger.info(`Server is Running on PORT ${serverConfig.PORT}`);
-    logger.info('Press Ctlr + C to stop the server.', {"fileName":"server.ts", "method":"listen", "line": 27});
+app.listen(serverConfig.PORT, async () => {
+  logger.info(`Server is Running on PORT ${serverConfig.PORT}`);
+  logger.info("Press Ctlr + C to stop the server.", {
+    fileName: "server.ts",
+    method: "listen",
+    line: 27,
+  });
+  try {
+    await sequelize.authenticate(); // Check if the database connection is successful
+    logger.info("Database connection has been established successfully.", {
+      fileName: "server.ts",
+      method: "listen",
+      line: 29,
+    });
+} catch (error) {
+    logger.error("Unable to connect to the database:", error, {
+      fileName: "server.ts",
+      method: "listen",
+      line: 32,
+    });
+  }
 });
